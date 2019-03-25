b953df8fc1921c1c3a10ee3e45e0686fe66980dc
Ingvar Stepanyan <me@rreverser.com>
2019-03-25 19:01:53 +0000

    Add serialize benchmark suite

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -3,7 +3,8 @@
 const { Suite } = require('benchmark');
 const benches = require('./pkg');
 
-let suite = new Suite();
+let parseSuite = new Suite('parse');
+let serializeSuite = new Suite('serialize');
 
 for (let input of ['canada', 'citm_catalog', 'twitter']) {
 	const json = require(`./${input}.json`);
@@ -11,11 +12,26 @@ for (let input of ['canada', 'citm_catalog', 'twitter']) {
 
 	for (const lib of ['serde_json', 'serde_wasm_bindgen']) {
 		const parse = benches[`parse_${input}_with_${lib}`];
-		suite.add(`${input} x ${lib}`, () => free(parse(json)));
+		parseSuite.add(`${input} x ${lib}`, () => free(parse(json)));
+
+		const serialize = benches[`serialize_${input}_with_${lib}`];
+		let parsed = parse(json);
+		serializeSuite.add(`${input} x ${lib}`, () => serialize(parsed), {
+			onComplete: () => free(parsed)
+		});
 	}
 }
 
-suite
-.on('error', event => console.error(event.target.error))
-.on('cycle', event => console.log(event.target.toString()))
-.run();
+function runSuite(suite) {
+	console.log('='.repeat(suite.name.length));
+	console.log(suite.name);
+	console.log('='.repeat(suite.name.length));
+
+	suite
+	.on('error', event => console.error(event.target.error))
+	.on('cycle', event => console.log(event.target.toString()))
+	.run();
+}
+
+runSuite(parseSuite);
+runSuite(serializeSuite);
--- benchmarks/src/lib.rs
+++ benchmarks/src/lib.rs
@@ -28,6 +28,16 @@ pub fn parse_canada_with_serde_json(input: JsValue) -> *const Canada {
 }
 
 #[wasm_bindgen]
+pub fn serialize_canada_with_serde_wasm_bindgen(input: *const Canada) -> JsValue {
+    serde_wasm_bindgen::to_value(unsafe { &*input }).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn serialize_canada_with_serde_json(input: *const Canada) -> JsValue {
+    JsValue::from_serde(unsafe { &*input }).unwrap()
+}
+
+#[wasm_bindgen]
 pub fn free_canada(input: *mut Canada) {
     unsafe { Box::from_raw(input); }
 }
@@ -43,6 +53,16 @@ pub fn parse_citm_catalog_with_serde_json(input: JsValue) -> *const CitmCatalog 
 }
 
 #[wasm_bindgen]
+pub fn serialize_citm_catalog_with_serde_wasm_bindgen(input: *const CitmCatalog) -> JsValue {
+    serde_wasm_bindgen::to_value(unsafe { &*input }).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn serialize_citm_catalog_with_serde_json(input: *const CitmCatalog) -> JsValue {
+    JsValue::from_serde(unsafe { &*input }).unwrap()
+}
+
+#[wasm_bindgen]
 pub fn free_citm_catalog(input: *mut CitmCatalog) {
     unsafe { Box::from_raw(input); }
 }
@@ -55,6 +75,16 @@ pub fn parse_twitter_with_serde_wasm_bindgen(input: JsValue) -> *const Twitter {
 #[wasm_bindgen]
 pub fn parse_twitter_with_serde_json(input: JsValue) -> *const Twitter {
     Box::leak(input.into_serde().unwrap())
+}
+
+#[wasm_bindgen]
+pub fn serialize_twitter_with_serde_wasm_bindgen(input: *const Twitter) -> JsValue {
+    serde_wasm_bindgen::to_value(unsafe { &*input }).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn serialize_twitter_with_serde_json(input: *const Twitter) -> JsValue {
+    JsValue::from_serde(unsafe { &*input }).unwrap()
 }
 
 #[wasm_bindgen]
