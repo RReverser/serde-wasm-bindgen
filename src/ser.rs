36434562a03ea31c902adde08c265768365c6238
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2021-06-04 13:04:39 +0200

    Factor ref out of match

--- src/ser.rs
+++ src/ser.rs
@@ -174,11 +174,11 @@ impl ser::SerializeMap for MapSerializer<'_> {
     fn serialize_value<T: ?Sized + Serialize>(&mut self, value: &T) -> Result<()> {
         let key = self.next_key.take().unwrap();
         let value_ser = value.serialize(self.serializer)?;
-        match self.target {
-            MapResult::Map(ref map) => {
+        match &self.target {
+            MapResult::Map(map) => {
                 map.set(&key, &value_ser);
             }
-            MapResult::Object(ref object) => {
+            MapResult::Object(object) => {
                 let obj: &Object = object.unchecked_ref();
                 obj.set(key, value_ser);
             }
