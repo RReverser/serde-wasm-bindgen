cec3fb51766cf8ff8b02325e8d87c8054cb9a973
mfish33 <32677537+mfish33@users.noreply.github.com>
2021-12-23 19:52:21 -0500

    Update src/de.rs
    Co-authored-by: Ingvar Stepanyan <me@rreverser.com>

--- src/de.rs
+++ src/de.rs
@@ -392,7 +392,7 @@ impl<'de> de::Deserializer<'de> for Deserializer {
             if &bindings::bigint_from_u64(converted_number) == bigint {
                 visitor.visit_u64(converted_number)
             } else {
-                Err(de::Error::custom("u64 attempted to be constructed from BigInt that was either larger than u64::MAX or less than u64::MIN"))
+                Err(de::Error::custom("Couldn't deserialize u64 from a BigInt outside u64::MIN..u64::MAX bounds"))
             }
         } else {
             self.deserialize_from_js_number_unsigned(visitor)
