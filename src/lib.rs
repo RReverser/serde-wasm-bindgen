68c5be4c42a6c1cba360259be1db36fad1cd660b
Ingvar Stepanyan <me@rreverser.com>
2022-05-12 13:54:33 +0100

    Simplify cache hasher
    Now that we're using `*const str` as a key, we don't need FNV to hash everything byte-by-byte, but instead can use the raw address of the start of the string as a hash directly. This is what I'm doing in a new custom hasher.
    
    This allows to drop dependency on fnv, very slightly improves performance, and slightly reduces code size.

--- src/lib.rs
+++ src/lib.rs
@@ -16,11 +16,37 @@ pub use ser::Serializer;
 type Result<T> = std::result::Result<T, Error>;
 
 fn static_str_to_js(s: &'static str) -> JsString {
+    use std::cell::RefCell;
+    use std::collections::HashMap;
+
+    #[derive(Default)]
+    struct PtrHasher {
+        addr: usize,
+    }
+
+    impl std::hash::Hasher for PtrHasher {
+        fn write(&mut self, _bytes: &[u8]) {
+            unreachable!();
+        }
+
+        fn write_usize(&mut self, addr_or_len: usize) {
+            if self.addr == 0 {
+                self.addr = addr_or_len;
+            }
+        }
+
+        fn finish(&self) -> u64 {
+            self.addr as _
+        }
+    }
+
+    type PtrBuildHasher = std::hash::BuildHasherDefault<PtrHasher>;
+
     thread_local! {
         // Since we're mainly optimising for converting the exact same string literal over and over again,
         // which will always have the same pointer, we can speed things up by indexing by the string's pointer
         // instead of its value.
-        static CACHE: std::cell::RefCell<fnv::FnvHashMap<*const str, JsString>> = Default::default();
+        static CACHE: RefCell<HashMap<*const str, JsString, PtrBuildHasher>> = Default::default();
     }
     CACHE.with(|cache| {
         cache
